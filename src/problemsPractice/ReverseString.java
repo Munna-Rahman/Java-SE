497ed4a5205cc20023ddd6a65ccc829b1c4e1773
Munna Rahman <munnarahman1994@gmail.com>
2018-10-24 11:21:10 +0600

    Reverse String toCharArray();

--- src/problemsPractice/ReverseString.java
+++ src/problemsPractice/ReverseString.java
@@ -1,12 +1,23 @@
 package problemsPractice;
 
+import java.util.Scanner;
+
 public class ReverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method s
 		
-		System.out.println("Java SE");
-		System.out.println("Code By Munna..!");
+		 Scanner scr = new Scanner(System.in);
+		  System.out.print("Input String :- ");
+		  
+		  char [] Letters = scr.nextLine().toCharArray();
+		  
+		  System.out.print("Revers String :- ");
+		  for(int i = Letters.length-1;i>=0;i--) {
+			  System.out.print(Letters[i]);
+		  }
+		  
+		  
 
 	}
 
